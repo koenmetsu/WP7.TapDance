0530f90f525eb8a9129d812a83b6ae98e5595473
Kevin <kevin.pelgrims@endomondo.com>
2012-07-21 09:37:17 +0200

    Added basic Game class

---
+++ WP7.TapDance/WP7.TapDance/Model/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace WP7.TapDance.Model
+{
+    public class Game : IGame
+    {
+        private readonly Stopwatch clickedWatch;
+
+        public Game()
+        {
+            clickedWatch = new Stopwatch();
+        }
+
+        public int[] GetNewPattern()
+        {
+            return new int[] { 1, 0, 2, 3 };
+        }
+
+        public double GetSecondsPassed()
+        {
+            return clickedWatch.Elapsed.TotalSeconds;
+        }
+    }
+}
