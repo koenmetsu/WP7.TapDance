12da41828d6fcf7faeafdb11b1d41ea94b182e59
Kevin <kevin.pelgrims@endomondo.com>
2012-07-21 12:04:32 +0200

    Removed some useless comments

--- WP7.TapDance/WP7.TapDance/ViewModel/MainViewModel.cs
+++ WP7.TapDance/WP7.TapDance/ViewModel/MainViewModel.cs
@@ -11,15 +11,6 @@ namespace WP7.TapDance.ViewModel
 {
     /// <summary>
     /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
@@ -34,14 +25,6 @@ namespace WP7.TapDance.ViewModel
         /// </summary>
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
             game = new Game();
             game.PlayerLost += GameOnPlayerLost;
             game.PlayerWon += GameOnPlayerWon;
