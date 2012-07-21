a21211c3e4de75f48147e6ebb1b11f975f05669d
koenmetsu <koen.metsu@gmail.com>
2012-07-21 09:21:45 +0200

    mockup code of the pattern

--- WP7.TapDance/WP7.TapDance/MainPage.xaml.cs
+++ WP7.TapDance/WP7.TapDance/MainPage.xaml.cs
@@ -19,19 +19,6 @@ namespace WP7.TapDance
         public MainPage()
         {
             InitializeComponent();
-
-            // Set the data context of the listbox control to the sample data
-            DataContext = App.ViewModel;
-            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
-        }
-
-        // Load data for the ViewModel Items
-        private void MainPage_Loaded(object sender, RoutedEventArgs e)
-        {
-            if (!App.ViewModel.IsDataLoaded)
-            {
-                App.ViewModel.LoadData();
-            }
         }
     }
 }
